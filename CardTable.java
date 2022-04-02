034be6212669519268cae40ea3e6d6ae80a1c03c
Roberto Arevalo <roberttt44@hotmail.com>
2022-04-01 18:14:45 -0700

    images and attempt 1 at cardTable

---
+++ CardTable.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CardTable extends JFrame {
+   static int MAX_CARDS_PER_HAND = 56;
+   static int MAX_PLAYERS = 2;  // for now, we only allow 2 person games
+
+   private int numCardsPerHand;
+   private int numPlayers;
+
+   public JPanel pnlComputerHand, pnlHumanHand, pnlPlayArea;
+
+   //constructor filters input, adds any panels to the JFrame, and
+   //establishs layouts
+   CardTable(String title, int numCardsPerHand, int numPlayers)
+      //get numCards...
+      //get numPlayers...
+}
+
